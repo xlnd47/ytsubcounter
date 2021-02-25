9d10835955bc0c9cb5566bc98a5af3c75291f5cb
Axel De Schuijmer <axel.deschuijmer@gmail.com>
2021-02-25 20:13:22 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ytsubcounter
+ simple
---
+++ index.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const getYoutubeSubscriber = require('getyoutubesubscriber');
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const { setIntervalAsync } = require('set-interval-async/dynamic');
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT | 3000;
+
+const adapter = new FileSync('db.json');
+const db = low(adapter);
+const moment = require('moment');
+
+db.defaults({ counts: [] }).write();
+
+let dbJson = require('./db.json');
+
+setIntervalAsync(
+    async () => {
+        let subcount = await getYoutubeSubscriber(process.env.YOUTUBE_CHANNEL_ID);
+        db.get('counts')
+            .push({ date: moment().format(), subcount: subcount})
+            .write()
+    },
+    process.env.POLLING_MINUTES * 60 * 1000 
+); 
+
+app.get('/', (req, res) => {
+    res.send(dbJson)
+});
+app.listen(port, () => {
+    console.log(`listening at port ${port}`)
+});
